bbe6c4b9660789035f21c43dbe43574646168cff
mfc <506321322@qq.com>
2019-01-03 07:50:12 +0800

    beta2

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -52,7 +52,7 @@ export default {
             updataList: 'upload/upadteList'
         }),
         onSelect(item) {
-            // 点击选项时默认不会关闭菜单，可以手动关
+            // 点击选项时默认不会关闭菜单，可以手动关闭
             this.show = false;
             console.log(item);
         },
